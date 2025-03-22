1c9614a03878d9779135f4e5780456d02ddbbd7a
Veronika Esparragoza <vesparragozam@gmail.com>
2025-03-22 22:39:34 +0100

    remove js files

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/BoozeAndBites-Vue/',
+  base: '/BoozeAndBites-Vue',
   plugins: [
     vue(),
     tailwindcss(),
